99277ce97fb390acd9d3bc1ebb843e56812adf0b
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-07 18:28:35 -0600

    Update CRUbotgit.py

--- CRUbotgit.py
+++ CRUbotgit.py
@@ -1,31 +1,33 @@
 # Crubot by dest
+# trollboxbot by dest
 import requests
 import discord
 import asyncio
-
 from discord.ext import commands
 from discord.ext.commands import Bot
 from discord import Game
 
+# "!" is the command trigger
 bot = commands.Bot(command_prefix='!')
-
+client = discord.Client()
+bot.remove_command("help")
+# on start up bot will print this data with its user name
 @bot.event
 async def on_ready():
     print ("011001100010001")
     print ("loading//.." + bot.user.name + ", Awaiting your command")
-    
+
+ # on member join will send a message to incoming user greeting them.. (still in progress may not work)
+ # dont forget to put the channel id# in the code too
 @bot.event
 async def on_member_join(member):
-    await bot.send_message(bot.get_channel('INSERTWELCOMECHANNELID'), " Welcome {0.mention}, I am the CRU Bot..Enjoy your time here. Please do your best to, message in the relevant chat server, and Respect one another. For a list of commands type !cmdlist" .format(member))
-    
-@bot.command(pass_context=True)
-async def ping(ctx):
-    await bot.say(":ping_pong:  " + bot.user.name + ", Awaiting your command $$$")
+    await bot.send_message(bot.get_channel('YOUNEEDCHANNELID#HERE'), " Welcome" + bot.user.name + " , at your service, type !help for a list of commands")
 
+#embed list of commands
 @bot.command(pass_context=True)
-async def cmdlist(ctx):
+async def help(ctx):
     embed = discord.Embed(title="The following are valid commands", color=0x42f4cb)
-    embed.add_field(name="!cmdlist", value="Get cmdlist message", inline=False)
+    embed.add_field(name="!help", value="Get help message", inline=False)
     embed.add_field(name="!ping", value="Get a bot responce", inline=False)
     embed.add_field(name="!cru", value="Get the price of Curium", inline=False)
     embed.add_field(name="!btc", value="Get the price of bitcoin", inline=False)
@@ -34,16 +36,44 @@ async def cmdlist(ctx):
     embed.add_field(name="!blockcount", value="Get the current CRU blockcount ", inline=False)
     embed.add_field(name="!hashrate", value="Get the current CRU Network Hashrate (h/s) ", inline=False)
     embed.add_field(name="!supply", value="Get the current CRU supply ", inline=False)
+    embed.add_field(name="!installguide", value="Get the Masternode install guide ", inline=False)
     embed.add_field(name="which coin gunna do goddest", value="type CRU", inline=False)
     await bot.say(embed=embed)
 
+# ping the bot command to test
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say(":ping_pong:  " + bot.user.name + ", Awaiting your command $$$")
+
+# get the latest masternode install guide
+@bot.command(pass_context=True)
+async def installguide(ctx):
+    await bot.say("Here is the latest masternode install guide version https://e-rave.nl/curium-master-node-setup-cold-wallet")
+
+# get the current price of btc
 @bot.command(pass_context=True)
 async def cru(ctx):
-    btcapi = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-    btcprice = requests.get(btcapi)
-    value = btcprice.json()['bpi']['USD']['rate']
-    await bot.say("Current Curium price is: $" + CRUprice)    
+    stockapi = 'https://api.coingecko.com/api/v3/coins/curium.json'
+    stockprice = requests.get(stockapi)
+    stockvalue = stockprice.json()['tickers'][0]['converted_last']['btc']
+    sxcvalue = stockprice.json()['tickers'][1]['converted_last']['btc']
+    chanvalue = stockprice.json()['tickers'][2]['converted_last']['btc']
+    stockvol = stockprice.json()['tickers'][0]['volume']  
+    sxcvol = stockprice.json()['tickers'][1]['volume']
+    chanvol = stockprice.json()['tickers'][2]['volume']
+    cruusdvalue = stockprice.json()['market_data']['current_price']['usd']
+    volvalue = stockprice.json()['market_data']['total_volume']['usd']
+    bvolvalue = stockprice.json()['market_data']['total_volume']['btc']
     
+    embed = discord.Embed(title="Here is price information for Curium", color=0x42f4cb)
+    embed.add_field(name="Price USD", value="The price is $" + str(cruusdvalue), inline=False)
+    embed.add_field(name="Price BTC On Stock.exchange", value="Price BTC " + str(stockvalue) + " Volume in CRU " + str(stockvol), inline=False)
+    embed.add_field(name="Price BTC On Crypto Hub", value="Price BTC " + str(chanvalue) + " Volume in CRU " + str(chanvol), inline=False)
+    embed.add_field(name="Price BTC On SouthXchange", value="Price BTC " + str(sxcvalue) + " Volume in CRU " + str(sxcvol), inline=False)
+    embed.add_field(name="Volume in USD ", value="This is the volume $" + str(volvalue), inline=False)
+    embed.add_field(name="Volume in BTC ", value="This is the volume BTC " + str(bvolvalue), inline=False)
+    await bot.say(embed=embed)
+
 @bot.command(pass_context=True)
 async def btc(ctx):
     btcapi = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
@@ -51,6 +81,7 @@ async def btc(ctx):
     value = btcprice.json()['bpi']['USD']['rate']
     await bot.say("Current Bitcoin price is: $" + value)
 
+# get the current price of ltc
 @bot.command(pass_context=True)
 async def ltc(ctx):
     url = 'https://api.coinmarketcap.com/v1/ticker/litecoin/'
@@ -58,6 +89,7 @@ async def ltc(ctx):
     value = response.json()[0]['price_usd']
     await bot.say("Current Litecoin price is: $" + value)
 
+# retun the current Currium difficulty
 @bot.command(pass_context=True)
 async def difficulty(ctx):
     ckapi = 'https://explorer.curiumofficial.com/api/getdifficulty'
@@ -65,6 +97,7 @@ async def difficulty(ctx):
     value = str(response)
     await bot.say("Current CRU difficulty:" + value)
 
+# return the current curium block count
 @bot.command(pass_context=True)
 async def blockcount(ctx):
     ckapi = 'https://explorer.curiumofficial.com/api/getblockcount'
@@ -72,6 +105,7 @@ async def blockcount(ctx):
     value = str(response)
     await bot.say("Current CRU blockcount:" + value)
 
+# return the current curium hashrate
 @bot.command(pass_context=True)
 async def hashrate(ctx):
     ckapi = 'https://explorer.curiumofficial.com/api/getnetworkhashps'
@@ -79,6 +113,7 @@ async def hashrate(ctx):
     value = str(response)
     await bot.say("Current CRU Network Hashrate (h/s):" + value)
 
+# return the current curium supply amount
 @bot.command(pass_context=True)
 async def supply(ctx):
     ckapi = 'https://explorer.curiumofficial.com/ext/getmoneysupply'
@@ -86,11 +121,37 @@ async def supply(ctx):
     value = str(response)
     await bot.say("Current CRU supply:" + value)
 
+# listen for someone to say cru then message them 
 @bot.listen()
 async def on_message(message):
     if message.content.startswith('cru'):
         userID = message.author.id
         await bot.send_message(message.channel, "<@%s> cha ching :moneybag: :dollar:" % (userID))
+        
+# alt way to get latest install guide
+@bot.listen()
+async def on_message(message):
+    if message.content.startswith('install-guide'):
+        userID = message.author.id
+        await bot.send_message(message.channel, "<@%s> Here is the latest Masternode install guide https://e-rave.nl/curium-master-node-setup-cold-wallet" % (userID))
+ 
+async def updateprice():
+    while True:
+        seapi_url = 'https://stocks.exchange/api2/ticker/'
+        seaapi_json = requests.get(seapi_url)
+        seaapi_res = seaapi_json.json()
+        price = 'Unknown'
+        for pair in seaapi_res:
+            if pair['market_name'] == 'CRU_BTC':
+                price = pair['last']
+        await client.change_presence(game=discord.Game(name="CRU: " + price))
+        await asyncio.sleep(60)
 
 
-bot.run("TYPEYOURTOKENHERE")
+
+
+
+
+        
+bot.loop.create_task(updateprice())
+bot.run("code thingy")
