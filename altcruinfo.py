9851665f63d54655efaf94a8f058abd8f7e3a6e4
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-10 21:42:34 -0600

    Update altcruinfo.py

--- altcruinfo.py
+++ altcruinfo.py
@@ -80,13 +80,12 @@ async def cru(ctx):
 async def scriv(ctx):
     gravapi = 'https://graviex.net/api/v2/tickers/scrivbtc.json'
     gravprice = requests.get(gravapi, verify=False)
-    gravy = json.loads(gravprice.content.strip(‘while(1);’))
     btcapi = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
     btcprice = requests.get(btcapi)
     btcvalue = btcprice.json()['bpi']['USD']['rate']
-    scrivvalue = gravy.json()['ticker']['last']
-    scrivbtcvol = gravy.json()['ticker']['volbtc']
-    scrivvol = gravy.json()['ticker']['vol']
+    scrivvalue = gravprice.json()['ticker']['last']
+    scrivbtcvol = gravprice.json()['ticker']['volbtc']
+    scrivvol = gravprice.json()['ticker']['vol']
     scrivusdvol = Decimal(scrivbtcvol) * Decimal(btcvalue)
     scrivusdvalue = Decimal(btcvalue) * Decimal(scrivvalue)
     
