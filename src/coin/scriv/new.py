5f98d94d2a60423832937b896e302d390b605006
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-31 13:50:56 -0600

    Create new.py

---
+++ src/coin/scriv/new.py
@@ -0,0 +1,21 @@
+import json
+import asyncio
+import requests
+
+
+def getInfoCRU():
+
+	
+    fileName = config.coinName['coin']
+    gravapi = 'https://graviex.net/api/v2/tickers/scrivbtc.json'
+    gravprice = requests.get(gravapi, verify=False)
+    scrivvol = gravprice.json()['ticker']['vol']
+
+	
+    data = {'test' : scrivvol}
+    
+	  with open("scriv.json", 'w') as fp:
+		    json.dump(data, fp)
+
+		
+		
