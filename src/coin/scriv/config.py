5b19542a11695507f8f3c4ceb0bf766597c30ae2
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-31 17:12:37 -0600

    Update config.py

--- src/coin/scriv/config.py
+++ src/coin/scriv/config.py
@@ -2,7 +2,7 @@ rpc_config = {
     "rpc_port": "22545",
     "rpc_host": "127.0.10.1",
     "rpc_username": "dogecoinrpc",
-    "rpc_password": "dogecoinpassword",
+    "rpc_password": "dogecoinpasword",
     "timeout": 60
 }
 
