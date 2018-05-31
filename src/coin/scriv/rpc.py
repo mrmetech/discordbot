bafe5b01095fbcef10f94fb3c0b6adb022c14d71
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-31 16:05:09 -0600

    Update rpc.py

--- src/coin/scriv/rpc.py
+++ src/coin/scriv/rpc.py
@@ -1,1 +1,8 @@
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import config
 
+def get_rpc():
+    return AuthServiceProxy("http://%s:%s@%s:%s" % (
+        config.rpc_config['rpc_username'], config.rpc_config['rpc_password'],
+        config.rpc_config['rpc_host'],
+        config.rpc_config['rpc_port']), timeout=config.rpc_config['timeout'])
