00214c62a1d4f90b2217f276069706baf1d045b8
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-31 17:16:19 -0600

    Update scrivInfo.py

--- src/coin/scriv/scrivInfo.py
+++ src/coin/scriv/scrivInfo.py
@@ -14,6 +14,17 @@ mncount = rpc.masternode('count')
 # let some daemon time to unlock wallet
 time.sleep(1)
 
+fileName = config.coinName['coin']
+gravapi = 'https://graviex.net/api/v2/tickers/scrivbtc.json'
+gravprice = requests.get(gravapi, verify=False)
+btcapi = 'https://api.coinmarketcap.com/v2/ticker/1/'
+btcprice = requests.get(btcapi)
+btcvalue = btcprice.json()['data']['quotes']['USD']['price']
+scrivvalue = gravprice.json()['ticker']['last']
+scrivbtcvol = gravprice.json()['ticker']['volbtc']
+scrivvol = gravprice.json()['ticker']['vol']
+scrivusdvol = float(scrivbtcvol) * float(btcvalue)
+scrivusdvalue = float(btcvalue) * float(scrivvalue)
 
 dailyEarningsUSD = (((config.blocksPerADay * mncount) * config.blockRewardForMasternodes) * scrivvalue * btcvalue)
 dailyEarningsBTC = (((config.blocksPerADay * mncount) * config.blockRewardForMasternodes) * scrivvalue)
@@ -31,17 +42,7 @@ yearlyEarningsUSD = dailyEarningsUSD * 365
 yearlyEarningsBTC = dailyEarningsBTC * 365
 yearlyEarningsCOIN = dailyEarningsCOIN * 365
 	
-fileName = config.coinName['coin']
-gravapi = 'https://graviex.net/api/v2/tickers/scrivbtc.json'
-gravprice = requests.get(gravapi, verify=False)
-btcapi = 'https://api.coinmarketcap.com/v2/ticker/1/'
-btcprice = requests.get(btcapi)
-btcvalue = btcprice.json()['data']['quotes']['USD']['price']
-scrivvalue = gravprice.json()['ticker']['last']
-scrivbtcvol = gravprice.json()['ticker']['volbtc']
-scrivvol = gravprice.json()['ticker']['vol']
-scrivusdvol = float(scrivbtcvol) * float(btcvalue)
-scrivusdvalue = float(btcvalue) * float(scrivvalue)
+
 	
 data = {'mncount' : mncount, 'dEUSD' : dailyEarningsUSD, 'dEBTC' : dailyEarningsBTC, 'dECOIN' : dailyEarningsCOIN, 'wEUSD' : weeklyEarningsUSD, 'wEBTC' : weeklyEarningsBTC, 'wECOIN' : weeklyEarningsCOIN, 'mEUSD' : monthlyEarningsUSD, 'mEBTC' : monthlyEarningsBTC, 'mECOIN' : monthlyEarningsCOIN, 'yEUSD' : yearlyEarningsUSD, 'yEBTC' : yearlyEarningsBTC, 'yCOIN' : yearlyEarningsCOIN, 'btcvalue' : btcvalue, 'scrivvalue' : scrivvalue, 'scrivbtcvol' : scrivbtcvol, 'scrivvol' : scrivvol, 'scrivusdvol' : scrivusdvol, 'scrivusdvalue' : scrivusdvalue}
     
