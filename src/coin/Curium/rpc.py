bb7f0be5d54eb0ab7c123b47866f6c02e8d62e5a
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-06-01 06:39:29 -0700

    good to go

---
+++ src/coin/Curium/rpc.py
@@ -0,0 +1,13 @@
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import config
+import json
+
+def get_rpc():
+    return AuthServiceProxy("http://%s:%s@%s:%s" % (
+        config.rpc_config['rpc_username'], config.rpc_config['rpc_password'],
+        config.rpc_config['rpc_host'],
+        config.rpc_config['rpc_port']), timeout=config.rpc_config['timeout'])
+def writeToJson(path, fileName, data):
+	filePathNameWExt = path + '/' + fileName + '.json'
+	with open(filePathNameWExt, 'w') as fp:
+		json.dump(data, fp, indent=4)
---
+++ src/coin/template/coinInfo.py
@@ -0,0 +1,55 @@
+from rpc import *
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import config
+import json
+import asyncio
+import requests
+import time
+
+
+rpc = get_rpc()
+
+mncount = rpc.masternode('count')
+
+# let some daemon time to unlock wallet
+time.sleep(1)
+
+fileName = config.coinName
+gravapi = 'https://graviex.net/api/v2/tickers/scrivbtc.json'
+gravprice = requests.get(gravapi, verify=False)
+btcapi = 'https://api.coinmarketcap.com/v2/ticker/1/'
+btcprice = requests.get(btcapi)
+btcvalue = btcprice.json()['data']['quotes']['USD']['price']
+scrivvalue = gravprice.json()['ticker']['last']
+scrivbtcvol = gravprice.json()['ticker']['volbtc']
+scrivvol = gravprice.json()['ticker']['vol']
+scrivusdvol = float(scrivbtcvol) * float(btcvalue)
+scrivusdvalue = float(btcvalue) * float(scrivvalue)
+
+dailyEarningsUSD = (((1 / float(mncount)) * float(config.blockReward)) * float(config.blocksPerADay) * float(config.blockRewardForMasternodes) * float(scrivvalue) * float(btcvalue))
+dailyEarningsBTC = (((1 / float(mncount)) * float(config.blockReward)) * float(config.blocksPerADay)  * float(config.blockRewardForMasternodes) * float(scrivvalue))
+dailyEarningsCOIN = ((1 / float(mncount)) *  float(config.blockReward) * float(config.blocksPerADay) * float(config.blockRewardForMasternodes))
+	
+weeklyEarningsUSD = dailyEarningsUSD * 7 
+weeklyEarningsBTC = dailyEarningsBTC  * 7
+weeklyEarningsCOIN = dailyEarningsCOIN * 7
+	
+monthlyEarningsUSD = weeklyEarningsUSD * 4
+monthlyEarningsBTC = weeklyEarningsBTC * 4
+monthlyEarningsCOIN = weeklyEarningsCOIN * 4
+	
+yearlyEarningsUSD = dailyEarningsUSD * 365 
+yearlyEarningsBTC = dailyEarningsBTC * 365
+yearlyEarningsCOIN = dailyEarningsCOIN * 365
+	
+
+	
+data = {'mncount' : mncount, 'dEUSD' : dailyEarningsUSD, 'dEBTC' : dailyEarningsBTC, 'dECOIN' : dailyEarningsCOIN, 'wEUSD' : weeklyEarningsUSD, 'wEBTC' : weeklyEarningsBTC, 'wECOIN' : weeklyEarningsCOIN, 'mEUSD' : monthlyEarningsUSD, 'mEBTC' : monthlyEarningsBTC, 'mECOIN' : monthlyEarningsCOIN, 'yEUSD' : yearlyEarningsUSD, 'yEBTC' : yearlyEarningsBTC, 'yCOIN' : yearlyEarningsCOIN, 'btcvalue' : btcvalue, 'scrivvalue' : scrivvalue, 'scrivbtcvol' : scrivbtcvol, 'scrivvol' : scrivvol, 'scrivusdvol' : scrivusdvol, 'scrivusdvalue' : scrivusdvalue}
+    
+path = config.path
+
+writeToJson(path, fileName, data)
+
+		
+		
+		
