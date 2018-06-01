7cf62fad0752426071bdfde4e9c33c683afa668c
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-06-01 11:54:24 -0600

    Update coinInfo.py

--- src/coin/Curium/coinInfo.py
+++ src/coin/Curium/coinInfo.py
@@ -13,21 +13,26 @@ mncount = rpc.masternode('count')
 
 # let some daemon time to unlock wallet
 time.sleep(1)
-
-fileName = config.coinName
-gravapi = 'https://graviex.net/api/v2/tickers/scrivbtc.json'
-gravprice = requests.get(gravapi, verify=False)
 btcapi = 'https://api.coinmarketcap.com/v2/ticker/1/'
 btcprice = requests.get(btcapi)
 btcvalue = btcprice.json()['data']['quotes']['USD']['price']
-scrivvalue = gravprice.json()['ticker']['last']
-scrivbtcvol = gravprice.json()['ticker']['volbtc']
-scrivvol = gravprice.json()['ticker']['vol']
-scrivusdvol = float(scrivbtcvol) * float(btcvalue)
-scrivusdvalue = float(btcvalue) * float(scrivvalue)
 
-dailyEarningsUSD = (((1 / float(mncount)) * float(config.blockReward)) * float(config.blocksPerADay) * float(config.blockRewardForMasternodes) * float(scrivvalue) * float(btcvalue))
-dailyEarningsBTC = (((1 / float(mncount)) * float(config.blockReward)) * float(config.blocksPerADay)  * float(config.blockRewardForMasternodes) * float(scrivvalue))
+fileName = config.coinName
+stockapi = 'https://api.coingecko.com/api/v3/coins/curium.json'
+stockprice = requests.get(stockapi)
+coinvalue = stockprice.json()['tickers'][0]['converted_last']['btc']
+sxcvalue = stockprice.json()['tickers'][1]['converted_last']['btc']
+chanvalue = stockprice.json()['tickers'][2]['converted_last']['btc']
+stockvol = stockprice.json()['tickers'][0]['volume']  
+sxcvol = stockprice.json()['tickers'][1]['volume']
+chanvol = stockprice.json()['tickers'][2]['volume']
+cruusdvalue = stockprice.json()['market_data']['current_price']['usd']
+volvalue = stockprice.json()['market_data']['total_volume']['usd']
+bvolvalue = stockprice.json()['market_data']['total_volume']['btc']
+usdvalue = float(btcvalue) * float(coinvalue)
+
+dailyEarningsUSD = (((1 / float(mncount)) * float(config.blockReward)) * float(config.blocksPerADay) * float(config.blockRewardForMasternodes) * float(coinvalue) * float(btcvalue))
+dailyEarningsBTC = (((1 / float(mncount)) * float(config.blockReward)) * float(config.blocksPerADay)  * float(config.blockRewardForMasternodes) * float(coinvalue))
 dailyEarningsCOIN = ((1 / float(mncount)) *  float(config.blockReward) * float(config.blocksPerADay) * float(config.blockRewardForMasternodes))
 mnroi = ((1 / float(mncount)) *  float(config.blockReward) * float(config.blocksPerADay) * float(config.blockRewardForMasternodes) * 365) / 1000
 	
@@ -45,7 +50,11 @@ yearlyEarningsCOIN = dailyEarningsCOIN * 365
 	
 
 	
-data = {'mnroi' : mnroi, 'mncount' : mncount, 'dEUSD' : dailyEarningsUSD, 'dEBTC' : dailyEarningsBTC, 'dECOIN' : dailyEarningsCOIN, 'wEUSD' : weeklyEarningsUSD, 'wEBTC' : weeklyEarningsBTC, 'wECOIN' : weeklyEarningsCOIN, 'mEUSD' : monthlyEarningsUSD, 'mEBTC' : monthlyEarningsBTC, 'mECOIN' : monthlyEarningsCOIN, 'yEUSD' : yearlyEarningsUSD, 'yEBTC' : yearlyEarningsBTC, 'yCOIN' : yearlyEarningsCOIN, 'btcvalue' : btcvalue, 'scrivvalue' : scrivvalue, 'scrivbtcvol' : scrivbtcvol, 'scrivvol' : scrivvol, 'scrivusdvol' : scrivusdvol, 'scrivusdvalue' : scrivusdvalue}
+data = {'mnroi' : mnroi, 'mncount' : mncount, 'dEUSD' : dailyEarningsUSD, 'dEBTC' : dailyEarningsBTC, 'dECOIN' : dailyEarningsCOIN,
+	'wEUSD' : weeklyEarningsUSD, 'wEBTC' : weeklyEarningsBTC, 
+	'wECOIN' : weeklyEarningsCOIN, 'mEUSD' : monthlyEarningsUSD, 'mEBTC' : monthlyEarningsBTC, 
+	'mECOIN' : monthlyEarningsCOIN, 'yEUSD' : yearlyEarningsUSD, 'yEBTC' : yearlyEarningsBTC, 'yCOIN' : yearlyEarningsCOIN, 
+	'btcvalue' : btcvalue, 'bvolvalue' : bvolvalue, 'volvalue' : volvalue, 'usdvalue' : usdvalue}
     
 path = config.path
 
