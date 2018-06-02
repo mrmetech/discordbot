1ef8405b446bf2db6f629e62050038498d39f4da
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-06-01 22:29:06 -0600

    adfded

---
+++ src/coin/phore/config.py
@@ -0,0 +1,17 @@
+rpc_config = {
+    "rpc_port": "22134",
+    "rpc_host": "127.0.12.1",
+    "rpc_username": "dogecoinrpc",
+    "rpc_password": "dogecoinpasword",
+    "timeout": 60
+}
+
+coinName = "curium"
+
+
+blocksPerADay = 1440
+
+blockRewardForMasternodes = 0.6
+blockReward = 5
+
+
