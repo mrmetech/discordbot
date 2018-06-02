25035d2b4e3faefc3a5a871a8590681af1d9d1ee
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-06-01 22:44:34 -0600

    Update getInfo.py

--- src/coin/getInfo.py
+++ src/coin/getInfo.py
@@ -3,8 +3,8 @@ import time
 
 while True:		
 
-	time.sleep(600)
 	Popen('python3 curium/coinInfo.py')
 	Popen('python3 gravium/coinInfo.py')
 	Popen('python3 scriv/coinInfo.py')
 	Popen('python3 phore/coinInfo.py')
+	time.sleep(600)
