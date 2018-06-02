54da4484bd1d7f58d3aedc52fb6c6df253723378
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-06-01 22:39:53 -0600

    Update config.py

--- src/coin/gravium/config.py
+++ src/coin/gravium/config.py
@@ -1,6 +1,6 @@
 rpc_config = {
-    "rpc_port": "22134",
-    "rpc_host": "127.0.12.1",
+    "rpc_port": "22154",
+    "rpc_host": "127.0.16.1",
     "rpc_username": "dogecoinrpc",
     "rpc_password": "dogecoinpasword",
     "timeout": 60
