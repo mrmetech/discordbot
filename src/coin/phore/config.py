a179e6c1734dd93476a5b6aea866641d41b80200
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-06-01 22:39:35 -0600

    Update config.py

--- src/coin/phore/config.py
+++ src/coin/phore/config.py
@@ -1,6 +1,6 @@
 rpc_config = {
-    "rpc_port": "22134",
-    "rpc_host": "127.0.12.1",
+    "rpc_port": "22144",
+    "rpc_host": "127.0.14.1",
     "rpc_username": "dogecoinrpc",
     "rpc_password": "dogecoinpasword",
     "timeout": 60
